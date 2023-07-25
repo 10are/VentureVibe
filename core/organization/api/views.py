cb846d869fdabc71cfafd9b727ff078d61f454d8
Onur-Yılmaz <onuryilmaz@trair.com.tr>
2023-07-26 00:52:36 +0300

    added Pagination for organizaiton model

--- core/organization/api/views.py
+++ core/organization/api/views.py
@@ -3,9 +3,13 @@ from .serializers import *
 from rest_framework import viewsets, pagination
 from rest_framework.response import Response
 
+class OrganizationPagination(pagination.PageNumberPagination):
+    page_size = 4
+    page_query_param = 'page'
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
 class OrganizationViewSet(viewsets.ModelViewSet):
     queryset = Organization.objects.all()
     serializer_class = OrganizationSerializer
-    pagination_class = pagination.PageNumberPagination
-
+    pagination_class = OrganizationPagination
